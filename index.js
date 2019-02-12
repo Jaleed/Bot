dbd84c802d2cb5314335aaf6b621682e4fab32f2
Jaleed <47573054+Jaleed@users.noreply.github.com>
2019-02-12 15:04:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -77,10 +77,10 @@ bot.on('message', (message) => {
             ];
             
             var rand = Math.floor(Math.random() * 20 + 1);
-            var rands = Math.floor(1 + ${b});
+            var rands = Math.floor(rand + b);
 
             message.delete()
-            message.channel.send(`You rolled a d20 + ${b}. \nYour rolled a ` + rand + `. \nYour result is ` + rands + `.`);
+            message.channel.send(`You rolled a d20 + ${b}. \nYou rolled a ` + rand + `. \nYour result is ` + rands + `.`);
         }
         
         if (command === "jaleed") {
